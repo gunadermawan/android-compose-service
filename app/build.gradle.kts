65b58024d27dd6bcf587a91084dbdf18eb1ab77d
gunadermawan <gunadermawan.official@gmail.com>
2023-08-06 22:23:00 +0700

    simulate stop self services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+//    coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 }
--- app/src/main/java/com/gunder/service/services/BackgroundService.kt
+++ app/src/main/java/com/gunder/service/services/BackgroundService.kt
@@ -4,9 +4,18 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.lang.UnsupportedOperationException
 
 class BackgroundService : Service() {
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     override fun onBind(intent: Intent): IBinder {
         throw UnsupportedOperationException("not yet implemented")
@@ -14,7 +23,21 @@ class BackgroundService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "onStartCommand: BackgroundService starting")
+        serviceScope.launch {
+            for (i in 1..50) {
+                delay(1000)
+                Log.d(TAG, "onStartCommand: Background service on $i")
+            }
+            stopSelf()
+            Log.d(TAG, "onStartCommand: BackgroundService stopped")
+        }
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+        Log.d(TAG, "onDestroy: BackgroundService stopped/Destroyed")
     }
 
     companion object {
